02cbda0ea679c83a4515047aafcf3008f138ea38
el-davo <david.ahern@emc.com>
2019-03-15 19:46:55 +0000

    Cleaning up

--- src/__tests__/acceptance/test-helper.ts
+++ src/__tests__/acceptance/test-helper.ts
@@ -9,11 +9,7 @@ let {Docker} = require('node-docker-api');
 export async function setupApplication(): Promise<AppWithClient> {
     const config = givenHttpServerConfig();
 
-    // Set host to `HOST` env var or ipv4 loopback interface
-    // By default, docker container has ipv6 disabled.
     config.host = config.host || process.env.HOST || '127.0.0.1';
-
-    // Set port to `PORT` env var or `0`
     config.port = config.port || +(process.env.PORT || 3000);
 
     const app = new WsFlareProjectApiApplication({
